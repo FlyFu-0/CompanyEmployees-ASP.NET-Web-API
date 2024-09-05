a03481395bbea13d80f55c833c1c63226c711640
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-05 22:10:59 +0300

    service layer finish

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service;
+using Service.Contracts;
 
 namespace CompanyEmployees.Extensions;
 
@@ -22,4 +26,10 @@ public static class ServiceExtensions
 
 	public static void ConfigureRepositoryManager(this IServiceCollection services)
 		=> services.AddScoped<IRepositoryManager, IRepositoryManager>();
+
+	public static void ConfigureServiceManager(this IServiceCollection services) =>
+		services.AddScoped<IServiceManager, ServiceManager>();
+
+	public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+		services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -11,6 +11,8 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddControllers();
 
