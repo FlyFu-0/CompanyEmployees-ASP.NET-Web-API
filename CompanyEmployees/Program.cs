b16ffdd3c6ca2606d4c3dc98922257c4bd5840e3
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-07 13:48:02 +0300

    support text/xml, 406 where not support

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -17,9 +17,12 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers()
-	.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference)
-	.Assembly);
+builder.Services.AddControllers(config =>
+{
+	config.RespectBrowserAcceptHeader = true;
+	config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters()
+.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
