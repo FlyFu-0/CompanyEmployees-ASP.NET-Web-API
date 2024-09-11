2d322fee7394623178924378ffe58a32ba467e0b
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-11 08:25:32 +0300

    add X-Pagination for frontend, refactor getemployees for big tables

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -17,11 +17,14 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
 	{
 		var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
 			.OrderBy(e => e.Name)
+			.Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+			.Take(employeeParameters.PageSize)
 			.ToListAsync();
 
-		return PagedList<Employee>
-				.ToPagedList(employees, employeeParameters.PageNumber,
-					employeeParameters.PageSize);
+		var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
+
+		return new PagedList<Employee>(employees, count,
+			employeeParameters.PageNumber, employeeParameters.PageSize);
 	}
 
 	public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges)
